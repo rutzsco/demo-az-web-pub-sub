88f5453949866ed471d29efa2b20cadae9471151
Scott Rutz <scrutz@microsoft.com>
2022-05-16 14:08:53 -0500

    cp

---
+++ Services/Demo.Subscriber/ProcessCompletedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Subscriber
+{
+    public class ProcessCompletedEvent
+    {
+        public Guid Id { get; set; }
+
+        public Guid ProcessId { get; set; }
+
+    }
+}
---
+++ Services/Demo.Subscriber/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using Azure.Messaging.WebPubSub;
+using System.Text.Json;
+using Websocket.Client;
+
+namespace Demo.Subscriber
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: subscriber <connectionString> <hub>");
+                return;
+            }
+            var connectionString = args[0];
+            var hub = args[1];
+
+            // Either generate the URL or fetch it from server or fetch a temp one from the portal
+            var serviceClient = new WebPubSubServiceClient(connectionString, hub);
+            var url = serviceClient.GetClientAccessUri();
+
+            using (var client = new WebsocketClient(url))
+            {
+                // Disable the auto disconnect and reconnect because the sample would like the client to stay online even no data comes in
+                client.ReconnectTimeout = null;
+                client.MessageReceived.Subscribe(msg => ProcessEvent(msg));
+                await client.Start();
+                Console.WriteLine("Connected.");
+                Console.Read();
+            }
+        }
+
+        private static void ProcessEvent(ResponseMessage responseMessage)
+        {
+            var pce = JsonSerializer.Deserialize<ProcessCompletedEvent>(responseMessage.Text);
+            Console.WriteLine($"Message received - Id: {pce.Id} ProcessId: {pce.ProcessId}");
+        }
+    }
+}
